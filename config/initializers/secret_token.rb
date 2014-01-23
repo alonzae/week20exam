616adfc14c167d1c5f7f0d926165c17de18a8761
alonzae <rushclass12>
2014-01-23 14:33:14 -0600

    hello
    this iv   fvndfnvnvnfnv

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Week20exam::Application.config.secret_key_base = 'cc5c9aa8a151c3de1c4f6e60160e778e878145d2d02de3260e5f110cafeb631690d5b8045a155871287cd9467a75d8560700de752c2f0081755ece15bb6a56ed'
